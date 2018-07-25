58a359bad47d73601807ca84dc6636e102c69c5a
WeijiaSun <waikasun@umich.edu>
2018-07-24 23:58:32 -0500

    More routes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,22 @@ import App from './app/App';
 import registerServiceWorker from './registerServiceWorker';
 
 //Custom imports
+import About from './app/views/About';
+import Projects from './app/views/Projects';
+import Resume from './app/views/Resume';
+import Stats from './app/views/Stats';
+import Contact from './app/views/Contact';
 import NotFound from './app/views/NotFound';
 
 ReactDOM.render(
     <Router>
       <Switch>
         <Route path="/" exact component={App} />
-        <Route path="/testroute" exact component={App} />
+        <Route path="/about" component={About} />
+        <Route path="/resume" component={Resume} />
+        <Route path="/projects" component={Projects} />
+        <Route path="/stats" component={Stats} />
+        <Route path="/contact" component={Contact} />
         <Route component={NotFound} status={404} />
       </Switch>
     </Router>, 
