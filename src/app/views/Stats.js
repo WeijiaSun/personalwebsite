3754121514cf34584c422e64277eb6d2860fdcfb
WeijiaSun <waikasun@umich.edu>
2018-07-27 00:27:15 -0500

    content

--- src/app/views/Stats.js
+++ src/app/views/Stats.js
@@ -20,12 +20,12 @@ const Stats = () => (
                     <td><Age /></td>   
                 </tr>
                 <tr>
-                    <td width="70%">Miles I have driven since licensed at 21st birthday</td>
+                    <td width="70%">Miles driven since 12/09/2013</td>
                     <td>{(124200 + ((Date.now() - new Date('2018-07-26T00:00:00')) / (24 * 60 * 60 *1000)) * 96).toFixed(0)}</td>   
                 </tr>
                 <tr>
                     <td width="70%"><a href="https://www.amcharts.com/visited_states/#US-AK,US-AL,US-AR,US-AZ,US-CA,US-CO,US-CT,US-DC,US-DE,US-FL,US-GA,US-HI,US-IL,US-IN,US-KS,US-KY,US-LA,US-MA,US-MD,US-MI,US-MN,US-MO,US-MS,US-NC,US-NE,US-NJ,US-NM,US-NV,US-NY,US-OH,US-OK,US-PA,US-TN,US-TX,US-UT,US-VA,US-WA,US-WY" target="_blank" rel="noopener noreferrer">
-                    States</a> I have been to</td>
+                    States</a> visited</td>
                     <td><a href="https://www.amcharts.com/visited_states/#US-AK,US-AL,US-AR,US-AZ,US-CA,US-CO,US-CT,US-DC,US-DE,US-FL,US-GA,US-HI,US-IL,US-IN,US-KS,US-KY,US-LA,US-MA,US-MD,US-MI,US-MN,US-MO,US-MS,US-NC,US-NE,US-NJ,US-NM,US-NV,US-NY,US-OH,US-OK,US-PA,US-TN,US-TX,US-UT,US-VA,US-WA,US-WY" target="_blank" rel="noopener noreferrer">
                     37</a></td>   
                 </tr>
