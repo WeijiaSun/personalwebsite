bc9f10b6af7f57561a36e8691a72e44f9703a398
Sun <Weijia_Sun@Dell.com>
2018-08-29 14:30:51 -0500

    remove embed pdf resume

--- src/app/views/Resume.js
+++ src/app/views/Resume.js
@@ -9,7 +9,6 @@ const Resume = () => (
             <div className="title">
             <h2>Resume</h2>
             <p>Click <a href="__files/WeijiaSunP.pdf" target="_blank" rel="noopener noreferrer">here</a> for Resume</p>
-            <div className="pdf"><embed src="__files/WeijiaSunP.pdf" width="900px" height="1150px" type='application/pdf'/> </div>
             </div>
         </header>
         </article>
