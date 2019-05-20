16fb288f1336f86699233ddc0eb41a901268c29b
WeijiaSun <iloveusomuch@me.com>
2019-05-19 21:36:18 -0500

    new resume and project page

--- src/app/views/Projects.js
+++ src/app/views/Projects.js
@@ -4,13 +4,19 @@ import App from '../App';
 
 const Projects = () => (
     <App>
-        <article className="post" id="index">
+        <article className="post">
         <header>
             <div className="title">
             <h2>Projects</h2>
             </div>
         </header>
         <div className="cell-container">
+            <article className="mini-post">
+                <header>
+                    <h3>Dell Design System</h3>
+                </header>
+                <a href="http://uicore-server-015.apps.cus.koaplatform.io/" target="_blank" rel="noopener noreferrer" className="image"><img src="/images/dds.png" alt=""/></a>
+            </article>
             <article className="mini-post">
                 <header>
                     <h3>Dell Standard Price List</h3>
